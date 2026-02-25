0206ab061e7973ba2f14f2ab088a65bc90b46f9e
x <x@yc>
2026-02-25 15:48:50 +0800

    init

---
+++ src/NoctMaple.ProtocolForge/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using NoctMaple.SDK.Abstractions;
+
+namespace NoctMaple.ProtocolForge
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Class1.Test();
+        }
+    }
+
+}
